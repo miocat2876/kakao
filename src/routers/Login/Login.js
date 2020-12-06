7b2760d94f5f766697ac06889d4148f8be9ef7c8
kyungmin kim <kkm1640@naver.com>
2020-12-06 18:51:13 +0900

    kim login

--- src/routers/Login/Login.js
+++ src/routers/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import './Login.css';
 import axios from 'axios';
 import kakaoLogo from '../../images/kakaoLogo.png';
@@ -7,11 +7,47 @@ const Login = ({ history }) => {
     const [login, setLogin] = useState(
         {
             id: "",
-            pw: ""
+            pw: "",
+            loginChk : false
         }
     );
-    
-    const { id, pw } = login;
+
+    const { id, pw, loginChk } = login;
+
+    const setLocal = () =>{
+        if(loginChk==true)
+            localStorage.setItem('login', JSON.stringify(login));
+        else
+            localStorage.setItem('login', JSON.stringify({id: "",pw: "",loginChk : false}));
+    }
+
+    const getLocal = (callback) =>{
+        let val = JSON.parse(localStorage.getItem('login'));
+        if(Object.keys(val).length>0){
+            console.log(val);
+            setLogin(val);
+            callback(val.loginChk);
+        }
+    }
+
+    useEffect(() => {
+        getLocal(function(loginChk){
+            setTimeout(() => {
+                if(loginChk===true){
+                    if(confirm("로그인 하시겠습니까?")){
+                        handleClick();
+                    }
+                }
+            }, 1000);
+        });
+        
+           
+        
+       
+
+    }, []);
+
+
 
     const goChats = (id) =>{
         history.push(
@@ -29,10 +65,12 @@ const Login = ({ history }) => {
             login
           )
           .then(function (response) {
-               if(response.data.return === "fail")
+               if(response.data.return !== "fail"){
+                    setLocal();
                     goChats(login.id);
-                else
+               }else{
                     alert('로그인정보가 일치하지 않습니다.');
+               }
           })
           .catch(function (error) {
                 console.log(error);
@@ -41,10 +79,18 @@ const Login = ({ history }) => {
 
     const handleChange = (e) => {
         const { value, name } = e.target;
-        setLogin({
-            ...login,
-            [name]: value
-        });
+        console.log(loginChk);
+        if(e.target.type =="checkbox"){
+            setLogin({
+                ...login,
+                [name]: e.target.checked
+            }); 
+        }else{
+            setLogin({
+                ...login,
+                [name]: value
+            });
+        }
     }
 
     const signUp = () => {
@@ -79,7 +125,7 @@ const Login = ({ history }) => {
                 </div>
                 <div id="login_auto">
                     <div id="loginAutoSection">
-                        <input type="checkbox"/><p>자동로그인</p>
+                        <input type="checkbox" name="loginChk" checked = {loginChk} onChange = {handleChange}/><p>자동로그인</p>
                     </div>
                     <a id="signUp" onClick={signUp}>회원가입</a>
                 </div>
