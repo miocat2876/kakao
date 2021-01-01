66f1a751731a543bcee0f459af4dcc89eb6775c4
yeonj21 <jiyeon.keum@gmail.com>
2021-01-01 14:00:52 +0900

    0101

--- src/routers/SignUp/SignUp.tsx
+++ src/routers/SignUp/SignUp.tsx
@@ -9,15 +9,15 @@ const SignUp = ({history}:any) =>{
         id: "",
         pw: "",
         pwCheck: "",
-        name: "",
+        nickName: "",
         phone: "",
         profile: ""
     });
 
-    const [valid, setValid] = useState('');
+    const [valid, setValid] = useState("");
     const [passwordCheck, setPasswordCheck] = useState('');
 
-    const { id, pw, pwCheck, name, phone, profile } = member;
+    const { id, pw, pwCheck, nickName, phone, profile } = member;
 
     const onChangeValue = (e:any) => {
         const { value, name } = e.target;
@@ -28,18 +28,17 @@ const SignUp = ({history}:any) =>{
         });
 
         if( id !== '' ){
-            console.log('input');
             if ( name === 'id' ){
-                axios.get('http://3.35.140.126:9000/user/duplicate_check?' + {id})
+                console.log(valid);
+                axios.get('http://3.35.140.126:9000/apis/users/duplicate_check?id=' + id)
                 .then(function(response){  
-                    console.log(response.data.result);
-                    if( response.data.result === 'success'){
-                        //중복 있음
-                        setValid('true');
+                    if(response.data === 'success'){ 
+                        //중복없음 사용가능
+                        setValid("true");
                     }
                     else{
-                        //중복 없음
-                        setValid('false');
+                        //중복, 사용불가
+                        setValid("false");
                     }
                 })
                 .catch(function(error){
@@ -54,7 +53,7 @@ const SignUp = ({history}:any) =>{
         ev.preventDefault();
         // props.onCreate(this.state);
         
-        if(!id || !pw || !name || !phone){ //전체 입력 확인
+        if(!id || !pw || !nickName || !phone){ //전체 입력 확인
             alert('모든 값이 입력되지 않았습니다.');
         }
         else{
@@ -70,7 +69,7 @@ const SignUp = ({history}:any) =>{
                         id: "",
                         pw: "",
                         pwCheck: "",
-                        name: "",
+                        nickName: "",
                         phone: "",
                         profile: ""
                     });
@@ -101,7 +100,7 @@ const SignUp = ({history}:any) =>{
         color: 'red',
         fontSize: '12px'
     }
-    //리턴에 영향을 주고 싶으면 state에 둬야한다.
+
     return(
         <main className="signup" >
                 <form className="form">
@@ -111,11 +110,12 @@ const SignUp = ({history}:any) =>{
                         </div>
                         <div className="container">
                             <Input placeholder="카카오계정" value={id} onChange={onChangeValue} name="id"></Input>
-                            <Input placeholder="비밀번호" value={pw} onChange={onChangeValue} name="pw"></Input>
-                            <Input placeholder="비밀번호 확인" value={pwCheck} onChange={onChangeValue} name="pwCheck"></Input>
+                            <Input type="password" placeholder="비밀번호" value={pw} onChange={onChangeValue} name="pw"></Input>
+                            <Input type="password" placeholder="비밀번호 확인" value={pwCheck} onChange={onChangeValue} name="pwCheck"></Input>
+                            <Input placeholder="닉네임" value={nickName} onChange={onChangeValue} name="nickName"></Input>
                             {(passwordCheck === 'false' && <div style={checkStyle}>입력하신 비밀번호 확인 부탁드립니다.</div>)}
                             <Input placeholder="휴대폰 번호 (ex. 010-0000-0000)" value={phone} onChange={onChangeValue} name="phone"></Input>
-                            <Input type="file" placeholder="프로필 사진" id="profile" name="profile"></Input>
+                            <input type="file" placeholder="프로필 사진" id="profile" name="profile"></input>
                         </div>
                         <Button type="submit" onClick={submit}>회원가입</Button>
                     </div>
@@ -136,9 +136,7 @@ const Input = styled.input`
     border: 1px solid gray;
     padding-left: 7px;
 
-    ${props => {
-        return props.valid ? 'background: blue' : 'background: red'
-    }}
+    
 `;
 
 const Button = styled.button`
