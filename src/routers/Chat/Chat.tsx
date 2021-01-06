8f7685b94e335b6465729c7606f833c4c3e4c899
kim <kkm1640@naver.com>
2021-01-06 21:37:00 +0900

    스크롤 수정

--- src/routers/Chat/Chat.tsx
+++ src/routers/Chat/Chat.tsx
@@ -103,8 +103,16 @@ var stompClient;
 
     //스크롤 페이징 함수
     function scrollCheck(){
+
         $('#chat_view').scroll(function(){   //스크롤이 최하단 으로 내려가면 리스트를 조회하고 page를 증가시킨다.
-            if($('#chat_view')[0].scrollHeight <= $('#chat_view')[0].offsetHeight + $('#chat_view').scrollTop()*-1){
+
+            console.log('asd');
+            console.log($('#chat_view')[0].scrollHeight );
+            console.log($('#chat_view')[0].offsetHeight);
+            console.log($('#chat_view'));
+            console.log($('#chat_view')[0].offsetHeight <= $('#chat_view')[0].offsetHeight - $('#chat_view').scrollTop());
+
+            if($('#chat_view')[0].offsetHeight <= $('#chat_view')[0].offsetHeight - $('#chat_view').scrollTop()){//$('#chat_view')[0].scrollHeight <= $('#chat_view')[0].offsetHeight + $('#chat_view').scrollTop()*-1
                 console.log('페이징');
 
                 paging(pageCheck);
